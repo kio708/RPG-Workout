84817c4babe100d187250e3fb3d5174eb9deb4d2
KIO-LAPTOP\oskar <oskarvengengel@gmail.com>
2024-03-22 15:44:01 +0100

    implemented better xp calculation

--- enter-workout-data.js
+++ enter-workout-data.js
@@ -1,16 +1,32 @@
 "use strict";
 
+class Run {
+    distance;
+    duration;
+    averageSpeed;
+    experiencePoints;
+    constructor(distance = NaN, duration = NaN) {
+        this.distance = distance;
+        this.duration = duration;
+        this.averageSpeed = Math.round(100*this.distance/this.duration)/100;
+        this.reCalculateExperiencePoints()
+    }
+
+    reCalculateExperiencePoints() {
+        this.experiencePoints = Math.round((this.distance*this.distance/this.duration)*100)/100;
+    }
+}
+
 const runDistanceInput = document.getElementById("run-distance");
 const runDurationInput = document.getElementById("run-duration")
 const runHistoryView = document.getElementById("previous-runs");
-const runHistoryData = [];
 
 function AddData() {
     event.preventDefault();
-    runHistoryData.push(runDistanceInput.value);
+    const runData = new Run(runDistanceInput.value, runDurationInput.value);
     runHistoryView.textContent +=
-        `${runDistanceInput.value}m in 
-        ${runDurationInput.value}s at a speed of 
-        ${runDistanceInput.value/runDurationInput.value}m/s and was worth
-        ${((runDistanceInput.value/runDurationInput.value)*(runDistanceInput.value+runDurationInput.value))/100}XP, ` //This Equation is bad
+        `${runData.distance}m in 
+        ${runData.duration}s at a speed of 
+        ${runData.averageSpeed}m/s and was worth
+        ${runData.experiencePoints}XP, `
 }
